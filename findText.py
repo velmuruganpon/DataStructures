b275a55514f7b2af63604455247bc90bbb88ce22
velmurugan ponnusamy <62397612+velmuruganpon@users.noreply.github.com>
2021-11-15 14:39:33 +0000

    deleted the findText.py

--- findText.py
+++
@@ -1,5 +0,0 @@
-def findText(xs: list, finStr : str) :
-  findStr =  [ i for i in xs if i == finStr ] 
-  lenFindXs = len(findStr)
-  if lenFindXs > 0:
-    print( ( "string found!!!" + "\n")  * lenFindXs )
